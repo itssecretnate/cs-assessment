1b72faa32359180e84992c6b1209a08a53a1ed3d
Nathan Mena <natemena153@gmail.com>
2022-03-25 10:45:40 -0600

    Added really bad pangram checker.

--- write-code.js
+++ write-code.js
@@ -26,20 +26,40 @@ addToZero([1, 2, 3, -2]);
 //  Unique Characters
 
 function hasUniqueChars(stringToCheck) {
-
     for(let i = 0; i < stringToCheck.length; i++) {
-
         for(let j = i + 1; j < stringToCheck.length; j++) {
             if(stringToCheck[i] == stringToCheck[j]) return false;
         }
+    }
+    return true;
+}
 
+
+hasUniqueChars("Monday");
+// -> True
+
+hasUniqueChars("Moonday");
+// -> False
+
+
+// Pangram Sentance
+function isPangram(sentence) {
+    sentence = sentence.toLowerCase();
+
+    let lowercaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+    const lettersArr = [];
+
+    for(let i = 0; i < sentence.length; i++) {
+      if(lowercaseLetters.includes(sentence[i]) && !lettersArr.includes(sentence[i])) lettersArr.push(sentence[i]);
     }
 
-    return true;
-  }
-  
-  hasUniqueChars("Monday");
-  // -> True
-  
-  hasUniqueChars("Moonday");
-  // -> False
+    if(lettersArr.length === 26) return true;
+    else return false;
+}
+
+isPangram("The quick brown fox jumps over the lazy dog!");
+// -> True
+
+isPangram("I like cats, but not mice");
+// -> False
